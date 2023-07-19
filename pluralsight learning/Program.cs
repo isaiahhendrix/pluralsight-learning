61768c20f3109a3f38fd152c621fbf5c3b29df2a
Zack Hendrix <Zackhendrix01@gmail.com>
2023-07-18 22:36:03 -0500

    Add project files.

---
+++ pluralsight learning/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+/*Console.WriteLine("Hello, everybody");
+
+Console.WriteLine("please enter your first name");
+String firstname = Console.ReadLine();
+
+Console.WriteLine("please enter your last name");
+string lastname = Console.ReadLine();
+
+Console.WriteLine("hello " + firstname +" " +lastname);*/
+
+//Console.WriteLine("welcome to bethany's pie shop");
+
+//Console.WriteLine("please enter your name: ");
+
+//string name = Console.ReadLine();
+
+//string name2
+
+//int monthlyWage = 1234;
+
+//int months = 12, bonus = 1000;
+
+//bool isActive = true;
+
+//double rating = 99.25;
+
+//byte numberOfEmployees = 300;
+
+//int hoursWorked; 
+
+//hoursWorked = 125;
+//hoursWorked =  148;
+////monthlyWage = true;
+
+//const double interestRate = 0.007;
+
+//string firstName = "bethany";
+//string lastName = "smith";
+//string emptyString = "";
+//Console.WriteLine("please enter your name");
+//string name = Console.ReadLine();
+
+//double ratePerHour = 12.34;
+//int numberOfHoursWorked = 165;
+
+//double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
+//Console.WriteLine(currentMonthWage);
+
+//ratePerHour += 3; //ratePerHour = RatePerHour +3
+//Console.WriteLine(ratePerHour);
+//if (currentMonthWage > 2000)
+//    Console.WriteLine("top paid employee!");
+//int numberOfEmployees = 15;
+
+//bool a;
+//int b;
+//numberOfEmployees--;
+
+//Console.ReadLine();
+
+int intMaxValue = int.MaxValue;
+int intMinValue = int.MinValue;
+char userSelection = 'a';
+char upperVersion = char.ToUpper(userSelection);
+bool isDigit = char.IsDigit(userSelection);
+
+bool isLetter = char.IsLetter(userSelection);
+
+int a;
+
+
+Console.ReadLine();
+
+
+
+
