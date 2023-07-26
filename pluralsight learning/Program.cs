1e5af357de47f25174f1a8ed1d3547bdee2a5385
Zack Hendrix <Zackhendrix01@gmail.com>
2023-07-25 22:16:23 -0500

    if/switch/loop changes

--- pluralsight learning/Program.cs
+++ pluralsight learning/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.SymbolStore;
 using System.Runtime.CompilerServices;
 
 /*Console.WriteLine("Hello, everybody");
@@ -93,15 +94,191 @@ Console.WriteLine("hello " + firstname +" " +lastname);*/
 //Console.WriteLine(endHour.ToShortDateString());
 
 
-int numberOfHoursWorked = 165;
+//int numberOfHoursWorked = 165;
 
-long veryLongMonth = numberOfHoursWorked; //works fine
+//long veryLongMonth = numberOfHoursWorked; //works fine
 
-double d = 123456789.0;
+//double d = 123456789.0;
 
-int x = (int)d; //forcing c# to accept
+//int x = (int)d; //forcing c# to accept
 
-int intVeryLongMonth = (int)veryLongMonth;
+//int intVeryLongMonth = (int)veryLongMonth;
+
+// a == b; both operands are equal
+//a != b; both operands are not equal
+//a >= b; a greater than b operand
+// a <= b; b greater than a operand
+// age == 45; true if value of age is equal to 45, otherwise false
+// age != 0; true if age is not equal to 0
+// && = if both equations are true, will return true but if one is not, it will return false
+// || (or) will return true if either one is true, false if both are not true
 
 
+//int age = 23;
 
+//bool a =  age == 23;
+
+//Console.WriteLine("age is 23: " + a);
+
+//bool b = age > 23;
+//Console.WriteLine("age is greater than 23: " + b);
+
+//bool c = (age >= 18) && (age <= 65);
+//Console.WriteLine("age is between 18 and 65: " + c);
+
+//int age1 = 16;
+//int age2 = 64;
+//bool d = (age1 >= 18) && (age2 <= 65);
+//Console.WriteLine("Age1 is greater than 18 AND age2 is less than 65: " + d); //false due to &&, only 1 true
+//bool e = (age1 >= 18) || (age2 <= 65);
+//Console.WriteLine("age1 is greater than 18 OR age2 is less than 65: " + e); // true due to || (or) one is true, other false
+
+//flow of execution = first statement, then the second, then so on
+
+//Console.WriteLine("Enter the age of the new candidate: ");
+//int age = int.Parse(Console.ReadLine());
+
+//if (age < 18)
+//{
+//    Console.WriteLine("too young to apply");
+//    Console.WriteLine("Send email to candidate.");
+//}
+//else if (age > 65)
+//{
+//    Console.WriteLine("Sorry, the selected age is too old");
+//    Console.WriteLine("Send email to candidate.");
+//}
+
+//else
+//{
+//    Console.WriteLine("great, you can now start with the application!");
+//}
+
+//DateTime today = DateTime.Now;
+//bool endOfMonthPaymentStarted = false;
+
+//if (today.Date.Day == 20)
+//{
+//    Console.WriteLine("Please start end-of-month employee payments");
+//}
+//else if (today.Date.Day >= 25 && !endOfMonthPaymentStarted)
+//{
+//    Console.WriteLine("Payments will be late!");
+
+//else is not required
+
+// switch statement
+//works for most data types but not float and double
+//case labels use a pattern: constant or relational
+//each case type must be unique
+//default can be placed anywhere, always executed last
+
+//switch(age)
+//{
+//    case < 18:
+//    case > 65:
+//        Console.WriteLine("sorry, your age is not within the range we are looking for");     
+//         Console.WriteLine("sorry, your age is not within the range we are looking for");
+//        break;
+//    case 23:
+//        Console.WriteLine("Wow, exactly what we are looking for");
+//        break;
+//    default:
+//        Console.WriteLine("great, you can now start with the application!");
+//        break;
+//}
+//Console.WriteLine("Choose the action you want to do: ");
+//Console.WriteLine("1. Add employee");
+//Console.WriteLine("2. update employee");
+//Console.WriteLine("3. delete employee");
+//Console.WriteLine("99. exit application");
+//string selectedAction = Console.ReadLine();
+
+//while (selectedAction != "99")
+//{
+
+//    switch (selectedAction)
+//    {
+//        case "1":
+//            Console.WriteLine("adding new employee...");
+//            break;
+//        case "2":
+//            Console.WriteLine("updating employee...");
+//            break;
+//        case "3":
+//            Console.WriteLine("deleting employee...");
+//            break;
+//        default:
+//            Console.WriteLine("invalid input");
+//            break;
+//    }
+//    Console.WriteLine("Choose the action you want to do: ");
+//    Console.WriteLine("1. Add employee");
+//    Console.WriteLine("2. update employee");
+//    Console.WriteLine("3. delete employee");
+//    Console.WriteLine("99. exit application");
+//    selectedAction = Console.ReadLine();
+//}
+
+// b = true;
+//while (b) will keep running until a break or a false
+
+
+//Console.WriteLine("Enter a value: ");
+//int max = int.Parse(Console.ReadLine());
+//int i = 0;
+
+//while(i < max)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+//Console.WriteLine("loop finished!");
+
+//int i = 0;
+
+//while (i > 0)
+//{
+//    Console.WriteLine(i);
+//    i--;
+
+//}
+//Console.WriteLine("Loop finished");
+
+//int i = 0;
+//int j = 0;
+
+//while (i < 10)
+//{
+
+//    while (j < 10)
+//    {
+
+//        Console.WriteLine("i: " + i + " j:" + j);
+//        j++;
+
+//    }
+//    j = 0;
+//    i++;
+
+//}
+
+//while (true)
+//{
+//    Console.WriteLine(DateTime.Now);
+//}
+Console.WriteLine("Enter a value: ");
+int max = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < max; i++)
+{
+  
+
+    if (i == 5)
+    {
+        Console.WriteLine("Bingo!" + i + " was found!");
+        break;
+    }
+    Console.WriteLine(i);
+
+}
